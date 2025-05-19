da9b4c441ea65b4bd844ab1c36581d2550f085f3
zhaolei <oizhaolei@gmail.com>
2025-05-19 12:42:51 +0900

    consume-body-in-extractor-or-middleware

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -4,11 +4,16 @@ use crate::repositories::{
     create_car_repository, create_part_repository, create_user_repository, run_migrations,
 };
 use crate::router::router;
+use axum::body::{Body, Bytes};
 use axum::extract::{MatchedPath, Request};
-use axum::{Extension, Router};
+use axum::middleware::Next;
+use axum::response::{IntoResponse, Response};
+use axum::{Extension, Router, middleware};
+use hyper::StatusCode;
 use std::sync::Arc;
 use tower_http::trace::TraceLayer;
 use tracing::info_span;
+use http_body_util::BodyExt;
 
 pub async fn create_app(config: &Config) -> Router {
     let _ = run_migrations(config).await;
@@ -39,8 +44,37 @@ pub async fn create_app(config: &Config) -> Router {
                 // logging of errors so disable that
                 .on_failure(()),
         )
+        .layer(middleware::from_fn(print_request_body))
         .layer(Extension(user_repository))
         .layer(Extension(car_repository))
         .layer(Extension(part_repository))
         .layer(Extension(cache))
+}
+
+// middleware that shows how to consume the request body upfront
+async fn print_request_body(request: Request, next: Next) -> Result<impl IntoResponse, Response> {
+    let request = buffer_request_body(request).await?;
+
+    Ok(next.run(request).await)
+}
+
+// the trick is to take the request apart, buffer the body, do what you need to do, then put
+// the request back together
+async fn buffer_request_body(request: Request) -> Result<Request, Response> {
+    let (parts, body) = request.into_parts();
+
+    // this won't work if the body is an long running stream
+    let bytes = body
+        .collect()
+        .await
+        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()).into_response())?
+        .to_bytes();
+
+    do_thing_with_request_body(bytes.clone());
+
+    Ok(Request::from_parts(parts, Body::from(bytes)))
+}
+
+fn do_thing_with_request_body(bytes: Bytes) {
+    tracing::debug!(body = ?bytes);
 }
